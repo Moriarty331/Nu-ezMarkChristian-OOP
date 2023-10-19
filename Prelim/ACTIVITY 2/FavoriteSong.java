351c81dbd321ab1216cc19b23ba49d3f5435c54a
unknown <OO328@ccs.local>
2023-10-19 17:57:39 +0100

    ADDED PRELIM ACTIVITIES

---
+++ Prelim/ACTIVITY 2/FavoriteSong.java
@@ -0,0 +1,13 @@
+public class FavoriteSong
+{
+   public static void main (String[] args)
+   {
+      // Title : Lord patawad by Bassilyo
+      System.out.println("Lord, patawad");
+		System.out.println("Pagkat ako'y makasalanan");
+		System.out.println("Makasalanang nilalang");
+		System.out.println("Lord, patawad");
+		System.out.println("Pagkat ako'y makasalanan");
+		System.out.println("Makasalanang nilalang");    
+   }
+}
